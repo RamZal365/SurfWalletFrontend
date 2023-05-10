e6292c63a81e2983c8dd30f3db7aa3f0e892f78c
Ramon Zalabardo <ramon.zalabardo.bosch@gmail.com>
2023-05-10 13:23:56 +0200

    Adding  to the main pages the three buttons to go to the otherones

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './components/Home.jsx';
-import Character from "./components/Character.jsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Surfboard from './components/Surfboards/Surfboard';
+import Surfboards from './components/Surfboards/Surfboards';
+import Wetsuit from './components/Wetsuits/Wetsuit';
+import Wetsuits from './components/Wetsuits/Wetsuits';
+import Spot from './components/Spots/Spot';
+import Spots from './components/Spots/Spots';
 
 function App() {
   return (
     <div className="container">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home></Home>}></Route>
-          <Route path='personaje/:id' element={<Character></Character>}></Route>
+          <Route path='/' element={<Home></Home>}> </Route>
+          
+          {/*Surfboards */}
+          <Route path='/surfboard/:id' element={<Surfboard></Surfboard>}> </Route>
+          <Route path='/surfboards' element={<Surfboards></Surfboards>}> </Route>
+
+          {/*Wetsuits */}
+          <Route path='/wetsuit/:id' element={<Wetsuit></Wetsuit>}> </Route>
+          <Route path='/wetsuits' element={<Wetsuits></Wetsuits>}> </Route>
+
+          {/*Spots */}
+          <Route path='/spot/:id' element={<Spot></Spot>}> </Route>
+          <Route path='/spots' element={<Spots></Spots>}> </Route>
         </Routes>
       </BrowserRouter>
     </div>
